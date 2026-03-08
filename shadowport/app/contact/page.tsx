9fb2abbacbf549d32b9b52fd6de35f07df623b50
ZhenNeo <abhi21vk@gamil.com>
2026-03-08 23:12:59 +0530

    gave some structure and folder management

---
+++ shadowport/app/contact/page.tsx
@@ -0,0 +1,8 @@
+export default function Contact() {
+  return (
+    <section>
+      <h2>Contact Me</h2>
+      <p>this is my contact details.</p>
+    </section>
+  )
+}
---
+++ shadowport/app/skills/page.tsx
@@ -0,0 +1,8 @@
+export default function Skills() {
+  return (
+    <section>
+      <h2>Skills</h2>
+      <p>this is skills.</p>
+    </section>
+  )
+}
